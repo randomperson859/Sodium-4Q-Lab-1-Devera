ca56a3b0ca468930cd65b071170511529fd87abb
randomperson859 <lrddevera29@src.pshs.edu.ph>
2026-03-11 12:02:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function userShop(){
+    let totalPrice = + ;
+    
+    let checkList = document.forms["check"]["grocery"].value;
+    document.forms["check"]["grocery"].value = totalPrice;
+    
+}
